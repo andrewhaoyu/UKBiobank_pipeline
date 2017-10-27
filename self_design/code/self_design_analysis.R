705af708749650ba75352b39f266c850e7a2ed58
Zhang <andrew.haoyu@gmail.com>
2017-10-27 17:11:50 -0400

    test ml

--- self_design/code/self_design_analysis.R
+++ self_design/code/self_design_analysis.R
@@ -20,7 +20,7 @@ pc.pheno <- pc.clean[,1:15]
 colnames(pc.pheno) <- c("pc1","pc2","pc3","pc4","pc5","pc6",
                         "pc7","pc8","pc9","pc10","pc11","pc12",
                         "pc13","pc14","pc15")
-first.row <- c(rep(0,3),rep("C",15),"B")
+#first.row <- c(rep(0,3),rep("C",15),"B")
 
 sample.snptest <- data.frame(sample.pheno,pc.pheno,bin1)
 
@@ -28,23 +28,45 @@ sample.snptest <- data.frame(sample.pheno,pc.pheno,bin1)
 ####demostrate the self design code
 ####setup your genetic data files
 library(devtools)
-library(bc2,lib.loc = '/dcl01/chatterj/data/tools/')
+#library(bc2,lib.loc = '/dcl01/chatterj/data/tools/')
 #install_github("andrewhaoyu/bc2", args = c('--library="/home/zhangh24/R/x86_64-pc-linux-gnu-library/3.4"'))
 install_github("andrewhaoyu/bc2", ref='development',args = c('--library="/users/hzhang1/R/x86_64-pc-linux-gnu-library/3.3"'))
+library(bc2)
+
+###here we only demostrate the self design code on one file
 file.name <- '/dcl01/chatterj/data/UKBiobank_pipeline/extract/result/extract_1.gen'
 
+temp <- system('wc -l /dcl01/chatterj/data/UKBiobank_pipeline/extract/result/extract_1.gen',intern=T)
+n.snps <- as.integer(gsub(" /dcl01/chatterj/data/UKBiobank_pipeline/extract/result/extract_1.gen","",temp))
+
+p.value.result <- rep(0,n.snps)
 con <- file(file.name)
 open(con)
-oneLine <- readLines(con,n=1)
-myVector <- strsplit(oneLine," ")
-snpid <- as.character(myVector[[1]][2])
-#snpid_result[i] <- snpid
-n <- nrow(sample.snptest)
-snpvalue <- rep(0,n)
 
-test <- unlist(myVector)
-snppro <- as.numeric(unlist(myVector)[7:length(myVector[[1]])])
+for(i in 1:n.snps){
+  oneLine <- readLines(con,n=1)
+  myVector <- strsplit(oneLine," ")
+  snpid <- as.character(myVector[[1]][2])
+  #snpid_result[i] <- snpid
+  n <- nrow(sample.clean)
+  snpvalue <- rep(0,n)
+  
+  test <- unlist(myVector)
+  snppro <- as.numeric(unlist(myVector)[7:length(myVector[[1]])])
+  
+  snpvalue <- convert(snppro,n)
+  
+  model <- glm(sample.snptest$bin1~snpvalue+sample.snptest$pc1+sample.snptest$pc2+sample.snptest$pc3+sample.snptest$pc4+sample.snptest$pc5+sample.snptest$pc6+sample.snptest$pc7+sample.snptest$pc8+sample.snptest$pc9+sample.snptest$pc10+sample.snptest$pc11+sample.snptest$pc12+sample.snptest$pc13+sample.snptest$pc14+sample.snptest$pc15,
+               family = 'binomial')
+  
+    
+}
+text <- NULL
+for(i in 1:15){
+  text <- paste0(text,"+sample.snptest$pc",i)
+}
 
-snpvalue <- convert(snppro,n)
-snpvalue <- snpvalue[idx.fil][idx.match]
-freq <- sum(snpvalue)/(2*n.sub)
+
+
+#snpvalue <- snpvalue[idx.fil][idx.match]
+#freq <- sum(snpvalue)/(2*n.sub)
